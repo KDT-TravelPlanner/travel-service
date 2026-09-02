e7d1519f94126accf31f22ba562689b6836f6d7c
aragonhyeok <aragonhyeok@khu.ac.kr>
2026-09-02 11:06:27 +0900

    refactor: use travel-common package (SCRUM-97)

--- src/main/kotlin/com/ktcloud/travelplanner/global/logging/RequestIdGenerator.kt
+++ src/main/kotlin/com/ktcloud/travelplanner/global/logging/RequestIdGenerator.kt
@@ -1,12 +1,3 @@
 package com.ktcloud.travelplanner.global.logging
 
-import java.util.UUID
-
-class RequestIdGenerator {
-	fun generate(): String = UUID.randomUUID().toString()
-
-	companion object {
-		const val HEADER_NAME = "X-Request-Id"
-		const val MDC_KEY = "requestId"
-	}
-}
+typealias RequestIdGenerator = com.ktcloud.travelplanner.common.logging.RequestIdGenerator
